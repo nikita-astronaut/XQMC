d329123a7736bcf8e87c81197bfcc4899f636436
nikita-astronaut <nikita.astronaut@gmail.com>
2019-11-15 11:53:27 +0100

    wip

--- code/wavefunction_vmc.py
+++ code/wavefunction_vmc.py
@@ -11,36 +11,16 @@ except ImportError:
     pass
 
 # TODO: make this parallel (multiprocessing / GPU-threads)
-
-class singlet_wavefunction(Object):
+class wavefunction(Object):
     def __init__(self, config):
         self.config = config
+        self.var_params = self._init_var_params()
+        self.U_matrix = self._construct_U_matrix()
 
     def get_O(self, base_state):
         '''
             O_i = \\partial{\\psi_i} / \\psi_i
         '''
-
-    def get_wavefunction(indexes, pairing, config, pfaffian = False):
-        A = np.zeros((len(indexes), len(indexes)))
-        for n1, idx1 in enumerate(indexes):
-            for n2, idx2 in enumerate(indexes):
-                A[n1, n2] = pairing.get_pairing_f(idx1, idx2)
-        # print(np.sum(np.abs(A + A.T)))
-        if not pfaffian:
-            return xp.linalg.det(A)  # if we are performing MC-sampling, we only need the WF
-
-
-# counter = 0
-# deltas = []
-# config = config()
-# pairing = models_vmc.on_site_and_nn_pairing(config)
-#for index1 in range(config.n_orbitals * config.n_sublattices * config.Ls ** 2 * 2):
-#    for index2 in range(config.n_orbitals * config.n_sublattices * config.Ls ** 2 * 2):
-#        delta = pairing.get_pairing_f(index1, index2)
-#        if delta != 0.0:
-#            counter += 1
-#            deltas.append(delta)
-
-#  A = get_wavefunction(np.random.choice(config.total_dof, config.total_dof // 2, replace = False), pairing, config)
-#print(A)
+    def _construct_U_matrix(self):
+        K = self.config.model(self.config.L, self.config.mu)
+        Delta = self.config.pairing(self.config.L)  # the magnitude of the pairing is not yet included here
