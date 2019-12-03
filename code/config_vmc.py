removed many prints | fixes mu-derivative bug | fixed the number of particles | multiprocessor mode on

code/config_vmc.py
@@ -7,27 +7,26 @@ import pairings
 
 class MC_parameters:
     def __init__(self):
-        self.Ls = 6  # spatial size, the lattice will be of size Ls x Ls
-        self.U = 4 # the force of on-site Coulomb repulsion in the units of t1
+        self.Ls = 10  # spatial size, the lattice will be of size Ls x Ls
+        self.U = 8 # the force of on-site Coulomb repulsion in the units of t1
         self.V = 0.  # the force of on-site Coulomb repulsion in the units of t1
         self.model = models.H_TB_Sorella_square
         self.n_orbitals = 1
-        self.mu = 0.0
+        self.mu = 1.0
         self.hamiltonian = hamiltonians_vmc.hamiltonian_2bands
         self.n_sublattices = 1
         self.MC_chain = 20000  # the number of spin flips starting from the initial configuration (can be used both for thermalization and generation)\
         self.MC_thermalisation = 20000
         self.total_dof = self.Ls ** 2 * 2 * self.n_sublattices * self.n_orbitals
-        self.particles_excess = 0
-        self.total_spin = 0
-        self.N_electrons = self.total_dof // 2
-        self.correlation = 10
+        self.N_electrons = 84 # only applied if PN_projection = True
+        self.correlation = 100
         self.optimiser = optimisers.AdamOptimiser
         self.opt_parameters = [0.5, 0.5, 1e-8, 1e-1]
         self.BC_twist = True  # whether to apply the BC--twise method (PBC in x direction and APBC in y direction)
-        self.initial_gap_parameters = np.array([0.35])
-        self.initial_jastrow_parameters = np.array([0.35])
+        self.PN_projection = True
+        self.initial_gap_parameters = np.array([0.01])
+        self.initial_jastrow_parameters = np.array([1.0])
 
 config = MC_parameters()
 pairings.obtain_all_pairings(config)
-config.pairings_list = [pairings.on_site_pairings_1orb_square[0]]
+config.pairings_list = [pairings.NN_pairings_1orb_square[1]]

code/models.py
@@ -99,7 +99,7 @@ def from_linearized_index(index, L, n_orbitals, n_sublattices = 2):
 def to_linearized_index(x, y, sublattice, orbit, L, n_orbitals, n_sublattices = 2):
     return orbit + n_orbitals * (sublattice + n_sublattices * (y + x * L))
 
-def H_TB_simple(config, only_NN = False):
+def H_TB_simple(config, mu, only_NN = False):
     t1, t2 = 0.331, (-0.010 + 1.0j * 0.097)
     if only_NN:
         t2 = 0.0 + 0.0j
@@ -125,10 +125,10 @@ def H_TB_simple(config, only_NN = False):
                     K[first, second] = -np.imag(t2)
 
     K = K + K.conj().T
-    K = K - config.mu * np.eye(K.shape[0])
+    K = K - mu * np.eye(K.shape[0])
     return apply_twisted_periodic_conditions(config, K)
 
-def H_TB_Sorella_hexagonal(config):
+def H_TB_Sorella_hexagonal(config, mu):
     t1 = 1.
     K = np.zeros((config.total_dof // 2, config.total_dof // 2))
     for first in range(config.total_dof // 2):
@@ -143,7 +143,7 @@ def H_TB_Sorella_hexagonal(config):
                 K[first, second] = t1
 
     K = K + K.conj().T
-    K = K - config.mu * np.eye(K.shape[0])
+    K = K - mu * np.eye(K.shape[0])
     return apply_twisted_periodic_conditions(config, K)
 
 def interorbital_mod(A, n_orbitals):
@@ -154,10 +154,9 @@ def interorbital_mod(A, n_orbitals):
 
 def get_adjacency_list(config, max_len):
     if config.n_sublattices == 2:
-        K_matrix = H_TB_Sorella_hexagonal(config)  # only nearest-neighbors
+        K_matrix = H_TB_Sorella_hexagonal(config, 0.0)  # only nearest-neighbors
     else:
-        K_matrix = H_TB_Sorella_square(config)  # only nearest-neighbors
-    K_matrix += config.mu * np.eye(K_matrix.shape[0])  # remove mu!
+        K_matrix = H_TB_Sorella_square(config, 0.0)  # only nearest-neighbors
 
     A = np.abs(np.asarray(K_matrix)) > 1e-6
 
@@ -172,7 +171,7 @@ def get_adjacency_list(config, max_len):
     return adjacency_list
 
 
-def H_TB_Sorella_square(config):
+def H_TB_Sorella_square(config, mu):
     t1 = 1.
     K = np.zeros((config.total_dof // 2, config.total_dof // 2))
     for first in range(config.total_dof // 2):
@@ -187,8 +186,7 @@ def H_TB_Sorella_square(config):
                 K[first, second] = t1 # * bc_factor
 
     # K = K + K.conj().T # already counted
-    print(np.sum(K))
-    K = K - config.mu * np.eye(K.shape[0])
+    K = K - mu * np.eye(K.shape[0])
     return apply_twisted_periodic_conditions(config, K)
 
 def apply_twisted_periodic_conditions(config, K):

code/monte_carlo_vmc.py
@@ -4,8 +4,9 @@ import models_vmc
 from wavefunction_vmc import wavefunction_singlet
 from config_vmc import config as config_vmc
 import pairings
-from tqdm import tqdm
-
+from joblib import Parallel, delayed
+import psutil
+n_cpus = psutil.cpu_count(logical = True) 
 xp = np
 try:
     import cupy as cp
@@ -13,31 +14,22 @@ try:
 except ImportError:
     pass
 
-def generate_MC_chain(hamiltinian, wavefunction):
-    for MC_step in tqdm(range(config_vmc.MC_thermalisation)):
+def get_MC_chain_result(hamiltonian, config_vmc, pairings_list, mu_parameter, gap_parameters, jastrow_parameters):
+    wavefunction = wavefunction_singlet(config_vmc, pairings_list, mu_parameter, gap_parameters, jastrow_parameters)
+
+    for MC_step in range(config_vmc.MC_thermalisation):
         wavefunction.perform_MC_step()
 
     energies = []
     Os = []
-    for MC_step in tqdm(range(config_vmc.MC_chain)):
+    for MC_step in range(config_vmc.MC_chain):
         if MC_step % config_vmc.correlation == 0:
-            energies.append(hamiltinian(wavefunction))
+            energies.append(hamiltonian(wavefunction))
             Os.append(wavefunction.get_O())
-            # print(np.mean(energies))
 
         wavefunction.perform_MC_step()
 
-    vol = wavefunction.config.total_dof // 2
-    energies = np.array(energies)
-    Os = np.array(Os)
-    Os_mean = np.mean(Os, axis = 0)
-    forces = -2 * (np.einsum('i,ik->k', energies.conj(), Os) / len(energies) - np.mean(energies) * Os_mean).real
-    print('<E> / t / vol =', np.mean(energies) / vol, '+/-', np.std(energies) / np.sqrt(len(energies)) / vol)
-    S_cov = (np.einsum('nk,nl->kl', (Os - Os_mean[np.newaxis, :]), (Os - Os_mean[np.newaxis, :])) / len(Os_mean)).real
-    print('|forces| =', np.sqrt(np.sum(forces ** 2)))
-    if np.linalg.det(S_cov) == 0:
-        return forces
-    return np.linalg.inv(S_cov).dot(forces)
+    return energies, Os
 
 pairings_list = config_vmc.pairings_list
 gap_parameters = config_vmc.initial_gap_parameters
@@ -49,25 +41,39 @@ opt = config_vmc.optimiser(config_vmc.opt_parameters)
 
 
 while True:
-    # np.random.seed(0)
-    print(config_vmc.n_orbitals)
-    wf = wavefunction_singlet(config_vmc, pairings_list, mu_parameter, gap_parameters, jastrow_parameters)
-    forces = generate_MC_chain(H, wf)
+    results = Parallel(n_jobs=n_cpus)(delayed(get_MC_chain_result)(config_vmc.hamiltonian(config_vmc), config_vmc, pairings_list, \
+                                                                   mu_parameter, gap_parameters, jastrow_parameters) for i in range(n_cpus))
+    
+    energies = np.concatenate([np.array(x[0]) for x in results], axis = 0)
+    Os = np.concatenate([np.array(x[1]) for x in results], axis = 0)
+    vol = config_vmc.total_dof // 2
+
+    Os_mean = np.mean(Os, axis = 0)
+    forces = -2 * (np.einsum('i,ik->k', energies.conj(), Os) / len(energies) - np.mean(energies) * Os_mean).real
+    print('<E> / t / vol =', np.mean(energies) / vol, '+/-', np.std(energies) / np.sqrt(len(energies)) / vol)
+    S_cov = (np.einsum('nk,nl->kl', (Os - Os_mean[np.newaxis, :]), (Os - Os_mean[np.newaxis, :])) / len(Os_mean)).real
+    print('|forces| =', np.sqrt(np.sum(forces ** 2)))
+    if np.linalg.det(S_cov) != 0:
+        forces = np.linalg.inv(S_cov).dot(forces)  # stochastic reconfiguration
+
     print(mu_parameter, gap_parameters, jastrow_parameters)
     step = opt.get_step(forces)
     print(forces, step)
-    H.reset()
+
     mu_parameter += step[0]
     gap_parameters += step[1:2]
     jastrow_parameters += step[2:]
 
+
+    
+'''
 for dt in np.logspace(-7, -8, 100):
     np.random.seed(13)
-    wf_1 = wavefunction_singlet(config_vmc, pairings_list, [1.0], [1.0])
+    wf_1 = wavefunction_singlet(config_vmc, pairings_list, [1.0], [1.0], [1.0])
     np.random.seed(13)
-    wf_2 = wavefunction_singlet(config_vmc, pairings_list, [1.0 + dt], [1.0 + dt])
-    print((wf_2.current_ampl - wf_1.current_ampl) / dt / (wf_1.current_ampl + wf_2.current_ampl) * 2., wf_1.get_O()[0] + wf_1.get_O()[1],
-          (-wf_2.current_ampl - wf_1.current_ampl) / dt / (wf_1.current_ampl - wf_2.current_ampl))  # log derivative calculated explicitly
+    wf_2 = wavefunction_singlet(config_vmc, pairings_list, [1.0 + dt], [1.0], [1.0])
+    print((wf_2.current_ampl - wf_1.current_ampl) / dt / (wf_1.current_ampl + wf_2.current_ampl) * 2., wf_1.get_O()[0],
+          2 * (-wf_2.current_ampl - wf_1.current_ampl) / dt / (wf_1.current_ampl - wf_2.current_ampl))  # log derivative calculated explicitly
     # print(wf_1.get_O_pairing(0))  # log devirative calculated from O_k formula
-
+'''
 # generate_MC_chain(wf)

code/wavefunction_vmc.py
@@ -28,7 +28,7 @@ class wavefunction_singlet(object):
         while True:
             self.occupied_sites, self.empty_sites, self.place_in_string = self._generate_configuration()
             self.U_tilde_matrix = self._construct_U_tilde_matrix()
-            if np.linalg.matrix_rank(self.U_tilde_matrix) == self.config.N_electrons:
+            if np.linalg.matrix_rank(self.U_tilde_matrix) == self.config.total_dof // 2:
                 break
         self.W_GF = self._construct_W_GF()
         self.current_ampl = np.linalg.det(self.U_tilde_matrix) * self.get_cur_Jastrow_factor()
@@ -46,7 +46,6 @@ class wavefunction_singlet(object):
         Jastrow_ratio = self.get_Jastrow_ratio(moved_site % (len(self.state) // 2), \
                                                empty_site % (len(self.state) // 2), \
                                                delta_alpha, delta_beta)
-
         det_ratio = self.W_GF[empty_site, self.place_in_string[moved_site]]
         return det_ratio * Jastrow_ratio
 
@@ -72,7 +71,7 @@ class wavefunction_singlet(object):
         return V
 
     def _construct_mu_V(self):
-        V = np.diag([1.0] * self.K.shape[0] + [-1.0] * self.K.shape[0]) * 1.0j
+        V = -np.diag([1.0] * self.K.shape[0] + [-1.0] * self.K.shape[0]) + 0.0j
         return V
 
     def _get_derivative(self, V):  # obtaining (6.99) from S. Sorella book
@@ -104,7 +103,7 @@ class wavefunction_singlet(object):
         return np.array(O)
 
     def _construct_U_matrix(self):
-        self.K = self.config.model(self.config)
+        self.K = self.config.model(self.config, self.var_mu)
         # print(np.sum(self.K))
         Delta = pairings.get_total_pairing_upwrapped(self.config, self.pairings_list_unwrapped, self.var_params_gap)
         T = np.zeros((2 * self.K.shape[0], 2 * self.K.shape[1])) * 1.0j
@@ -117,8 +116,8 @@ class wavefunction_singlet(object):
         assert(np.allclose(np.diag(E), U.conj().T.dot(T).dot(U)))  # U^{\dag} T U = E
         self.U_full = deepcopy(U)
         self.E = E
-        lowest_energy_states = np.argpartition(E, self.config.N_electrons)[:self.config.N_electrons]  # select lowest-energy orbitals
-        print('energy of filled states =', np.sum(self.E[lowest_energy_states]))
+        lowest_energy_states = np.argpartition(E, self.config.total_dof // 2)[:self.config.total_dof // 2]  # select lowest-energy orbitals
+        # print('energy of filled states =', np.sum(self.E[lowest_energy_states]))
         rest_energies = E[np.setdiff1d(np.arange(len(self.E)), lowest_energy_states)]
 
         U = U[:, lowest_energy_states]  # select only occupied orbitals
@@ -130,9 +129,9 @@ class wavefunction_singlet(object):
             # for E, c in zip(Es, counts):
             #     print(E, c)
             # print(np.sort(self.E), np.unique(np.around(self.E * 1e+5), return_counts = True))
-            exit(-1)
-        else:
-            print('Closed shell configuration, gap =', rest_energies.min() - self.E_fermi)
+            # exit(-1)
+        #else:
+            #print('Closed shell configuration, gap =', rest_energies.min() - self.E_fermi)
             # Es, counts = np.unique(np.around(self.E * 1e+5), return_counts = True)
             # for E, c in zip(Es, counts):
             #     print(E, c)
@@ -147,9 +146,16 @@ class wavefunction_singlet(object):
         U_tilde_inv = np.linalg.inv(self.U_tilde_matrix)
         return self.U_matrix.dot(U_tilde_inv)
 
+    # n_up = N/2 - k
+    # n_down = N/2 - k
+    # n_up_tilde = N/2 - k
+    # n_down_tilde = N - n_down = N/2 + k
     def _generate_configuration(self):
-        occupied_sites = np.random.choice(np.arange(self.config.total_dof), size = self.config.N_electrons, replace = False)  ## FIXME: DEBUG!!!
-        # occupied_sites = np.arange(self.config.N_electrons) * 2 + 1
+        doping = (self.config.total_dof // 2 - self.config.N_electrons) // 2  # k
+        occupied_sites_particles = np.random.choice(np.arange(self.config.total_dof // 2), size = self.config.total_dof // 4 - doping, replace = False)
+        occupied_sites_holes = np.random.choice(np.arange(self.config.total_dof // 2, self.config.total_dof), size = self.config.total_dof // 4 + doping, replace = False)
+        occupied_sites = np.concatenate([occupied_sites_particles, occupied_sites_holes])
+
         place_in_string = (np.zeros(self.config.total_dof) - 1).astype(np.int64)
         place_in_string[occupied_sites] = np.arange(len(occupied_sites))
         self.state = np.zeros(self.config.total_dof, dtype=np.int64)
@@ -164,10 +170,18 @@ class wavefunction_singlet(object):
         return occupied_sites, empty_sites, place_in_string
 
     def perform_MC_step(self):
-        moved_site_idx = np.random.choice(np.arange(len(self.occupied_sites)), 1)[0]
-        moved_site = self.occupied_sites[moved_site_idx]
-        empty_site_idx = np.random.choice(np.arange(len(self.empty_sites)), 1)[0]
-        empty_site = self.empty_sites[empty_site_idx]
+        conserving_move = False
+        while not conserving_move:
+            moved_site_idx = np.random.choice(np.arange(len(self.occupied_sites)), 1)[0]
+            moved_site = self.occupied_sites[moved_site_idx]
+            empty_site_idx = np.random.choice(np.arange(len(self.empty_sites)), 1)[0]
+            empty_site = self.empty_sites[empty_site_idx]
+            if self.config.PN_projection:
+                if (moved_site > len(self.state) // 2 and empty_site > len(self.state) // 2) or \
+                   (moved_site <= len(self.state) // 2 and empty_site <= len(self.state) // 2):
+                   conserving_move = True
+            else:
+                conserving_move = True
 
         det_ratio = self.W_GF[empty_site, moved_site_idx]
 
@@ -188,6 +202,8 @@ class wavefunction_singlet(object):
         self.state[moved_site] = 0
         self.state[empty_site] = 1
         self.occupancy = self.state[:len(self.state) // 2] - self.state[len(self.state) // 2:]
+        # print(sum(self.occupancy))
+
         ### DEBUG ###
         # U_tilde_new = self._construct_U_tilde_matrix()
         # det_ratio_naive = np.linalg.det(U_tilde_new) ** 2 / np.linalg.det(self.U_tilde_matrix) ** 2