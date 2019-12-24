dcf5e7734eb57ccb247681a84174e2dc6127c8a4
Nikita Astrakhantsev <nikita.astronaut@gmail.com>
2019-12-24 14:53:19 +0100

    real observables

--- code/observables_vmc.py
+++ code/observables_vmc.py
@@ -11,13 +11,13 @@ def gap_gap_correlator(state, pairing):
                                   = \\sum\\limits_{ijkl} \\Delta_{ij}^* \\Delta_{kl} d_{j + L} d^{\\dag}_{i} d_k d^{\\dag}_{l + L}.
     '''
 
-    correlator = 0.0 + 0.0j
+    correlator = 0.0
     for i in range(pairing.shape[0]):
         for j in np.where(pairing[i, :] != 0.0)[0]:
             for k in range(pairing.shape[0]):
                 for l in np.where(pairing[k, :] != 0.0)[0]:
-                    correlator += wavefunction_vmc.get_wf_ratio_double_exchange(*state, i, j, k, l) * \
-                                  np.conj(pairing[i, j]) * pairing[k, l]
+                    correlator += np.real(wavefunction_vmc.get_wf_ratio_double_exchange(*state, i, j, k, l) * \
+                                          np.conj(pairing[i, j]) * pairing[k, l])
 
     # normalize pairing for the number of bonds coming from every site
     correlator /= np.sum(np.abs(pairing[0, :]))
