807bd62307f799cefa6231bcd6e3dd3e7c7f255b
nikita-astronaut <nikita.astronaut@gmail.com>
2020-01-22 01:07:58 +0100

    J

--- code/submit_script.py
+++ code/submit_script.py
@@ -2,7 +2,7 @@ import subprocess
 import os
 import sys
 
-U, V, fugacity = [float(x) for x in sys.argv[5:]]
+U, V, J, fugacity = [float(x) for x in sys.argv[5:]]
 bare_config = open(sys.argv[1], 'r')
 path_to_configs = sys.argv[2]
 path_to_sbatch = sys.argv[3]
@@ -14,10 +14,12 @@ for i, line in enumerate(lines):
         lines[i] = '        self.U = np.array([{:.2f}])\n'.format(U)
     if 'self.V =' in line:
         lines[i] = '        self.V = np.array([{:.2f}])\n'.format(V)
+    if 'self.J =' in line:
+        lines[i] = '        self.J = np.array([{:.2f}])\n'.format(J)
     if 'self.fugacity =' in line:
         lines[i] = '        self.fugacity = np.array([{:.2f}])\n'.format(fugacity)
 
-config_name = os.path.join(path_to_configs, 'config_U_{:.2f}_V_{:.2f}_f_{:.2f}.py'.format(U, V, fugacity))
+config_name = os.path.join(path_to_configs, 'config_U_{:.2f}_V_{:.2f}_J_{:.2f}_f_{:.2f}.py'.format(U, V, J, fugacity))
 for i, s in enumerate(config_name):
     if s == '.' and config_name[i:] != '.py':
         config_name[i] = '-'
@@ -31,11 +33,11 @@ lines = [line for line in sbatch_file]
 
 for i, line in enumerate(lines):
     if '#SBATCH -o' in line:
-        lines[i] = '#SBATCH -o {:s}/output_U_{:.2f}_V_{:.2f}_f_{:.2f}.out\n'.format(U, V, fugacity)
+        lines[i] = '#SBATCH -o {:s}/output_U_{:.2f}_V_{:.2f}_J_{:.2f}_f_{:.2f}.out\n'.format(U, V, J, fugacity)
     if '#SBATCH -e' in line:
-        lines[i] = '#SBATCH -e {:s}/error_U_{:.2f}_V_{:.2f}_f_{:.2f}.err\n'.format(U, V, fugacity)
+        lines[i] = '#SBATCH -e {:s}/error_U_{:.2f}_V_{:.2f}_J_{:.2f}_f_{:.2f}.err\n'.format(U, V, J, fugacity)
     if '#SBATCH --job-name' in line:
-        lines[i] = '#SBATCH --job-name U_{:.2f}_V_{:.2f}_f_{:.2f}\n'.format(U, V, fugacity)
+        lines[i] = '#SBATCH --job-name U_{:.2f}_V_{:.2f}_J_{:.2f}_f_{:.2f}\n'.format(U, V, J, fugacity)
 
     if 'python' in line:
         pieces = line.split()
