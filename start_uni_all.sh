fd638912ce3fd01dcb9cd7329fd9dcb16377c98c
Nikita Astrakhantsev <nastrakh@r033c01s05.galileo.cineca.it>
2020-06-24 18:31:00 +0200

    wip

--- start_uni_all.sh
+++ start_uni_all.sh
@@ -1,9 +1,11 @@
 #for n in 72 84 94 114 124 134 144 154 164 174 184 194 204 216
 #for n in 110 112 114 116 118
-for eps in 0.30; do
+path_logs="/gpfs/scratch/userexternal/nastrakh/"
+path="/galileo/home/userexternal/nastrakh/XQMC/"
+for eps in 3.00; do
     for xi in 2.0 1.5 1.0 0.7 0.3 0.1; do
-        for n in 136; do
-            python3 ./code/submit_script.py /s/ls4/users/astrakhantsev/DQMC_TBG/code/config_vmc.py /s/ls4/users/astrakhantsev/DQMC_TBG/config_files/ /s/ls4/users/astrakhantsev/DQMC_TBG/launch_scripts/launch_generated.sh /s/ls4/users/astrakhantsev/DQMC_TBG/logs/ $eps $xi $1 $n
+        for n in 124 128 132 136 152 156 160 164; do
+            python3 ./code/submit_script.py $path/code/config_vmc.py $path/config_files/ $path/launch_scripts/launch_generated.sh $path_logs/logs/ $eps $xi $1 $n
         done
     done
 done
